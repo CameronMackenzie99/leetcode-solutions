e21592a6de0ad552afed5c50374a462f49ccd2ba
CameronMackenzie99 <mackencameron@gmail.com>
2021-08-13 14:32:23 +0100

    first_non_repeating char Added more formal docstrings and a third function with a linear time complexity utilising a hashmap

--- first_non_repeating_character.py
+++ first_non_repeating_character.py
@@ -3,15 +3,19 @@
 
 
 def first_non_repeating_char(string):
-    """ Function that outputs the first non-repeating character in a string of lowercase english letters. 
+    """Given a string of lowercase english letters, returns the first non-repeating character in the string. If none, returns "_".
+
+    :param string: str
+    :return: str
+
     >>> first_non_repeating_char("aaabcccdeeef")
     'b'
     >>> first_non_repeating_char("abcbad")
     'c'
     >>> first_non_repeating_char("abcabcabc")
     '_'
-    >>> # no non-repeating characters so returns an underscore
     """
+    # (O)n^2 - nested double for loop so O(n*n). Less memory usage as count is not stored for each iteration
     charstring = list(str(string))
 
     for i in charstring:
@@ -25,18 +29,19 @@ def first_non_repeating_char(string):
 
 def first_non_repeating_char_2(string):
 
-    """ Function that outputs the first non-repeating character in a string of lowercase english letters. 
+    """Given a string of lowercase english letters, returns the first non-repeating character in the string. 
 
-    
+    :param string: str
+    :return: str
+
     >>> first_non_repeating_char_2("aaabcccdeeef")
     'b'
     >>> first_non_repeating_char_2("abcbad")
     'c'
     >>> first_non_repeating_char_2("abcabcabc")
     '_'
-    >>> # no non-repeating characters so returns an underscore
     """
-
+    # (O)n^2 - nested double for loop so O(n*n). Lower complexity floor Ω(n) than first_non_repeating_char as inner for loop has break condition for any count over one.
     charstring = list(str(string))
 
     for i in range(len(charstring)):
@@ -47,6 +52,33 @@ def first_non_repeating_char_2(string):
                 break
         if seen_duplicate == False:
             return charstring[i]
+    return "_" 
+
+def first_non_repeating_char_3(string):
+    """Given a string of lowercase english letters, returns the first non-repeating character in the string. 
+
+    :param string: str
+    :return: str
+
+    >>> first_non_repeating_char_3("aaabcccdeeef")
+    'b'
+    >>> first_non_repeating_char_3("abcbad")
+    'c'
+    >>> first_non_repeating_char_3("abcabcabc")
+    '_'
+    """
+    # O(2n) ~= O(n) as upper bound of complexity is looping through the string twice.
+    charstring = {}
+
+    for i in range(len(string)):
+        if string[i] not in charstring:
+            charstring[string[i]] = 1
+        else:
+            charstring[string[i]] += 1
+
+    for char in string:
+        if charstring[char] == 1:
+            return char
     return "_" 
 
 if __name__ == "__main__":
